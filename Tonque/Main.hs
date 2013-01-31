54c6ef607ba220a4fecce818ec6bbecfb4de5da6
daimatz <dai@daimatz.net>
2013-01-31 12:00:41 +0900

    use liftIO

--- Tonque/Main.hs
+++ Tonque/Main.hs
@@ -2,6 +2,7 @@
 
 import Tonque.BBS
 
+import Control.Monad.IO.Class
 import System.Environment (getEnv)
 import Data.Monoid (mconcat)
 import qualified Data.Text as T
@@ -12,9 +13,9 @@ import Web.Scotty
 main :: IO ()
 main = do
   port <- read <$> getEnv "PORT"
-  bbss <- allBBSListHTML
   scotty port $ do
     get "/" $ do
+      bbss <- liftIO allBBSListHTML
       html $ TL.pack $ T.unpack bbss
     get "/bbs" $ do
       html "OK"
